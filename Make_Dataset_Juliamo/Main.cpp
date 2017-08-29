1e91307ccd3886bf518bbfbc495daa68c67e89cd
sugurunatsuno <suguru.irie@gmail.com>
2017-08-30 00:40:27 +0900

    second commit

--- Make_Dataset_Juliamo/Main.cpp
+++ Make_Dataset_Juliamo/Main.cpp
@@ -1,14 +1,66 @@
-﻿
-# include <Siv3D.hpp>
+﻿# include <Siv3D.hpp>
 
 void Main()
 {
-	const Font font(30);
+	Window::Resize(280, 280);
+
+	Image image(280, 280, Palette::Black);
+	Image dstImage(28 * 7, 28 * 4, Palette::Black);
+	Image temp(28, 28, Palette::Black);;
+
+	DynamicTexture texture(image);
+
+	Array<Image> alhpabet;
+	int32 alphabets = 28;
+	int32 count = 0;
 
 	while (System::Update())
 	{
-		font(L"ようこそ、Siv3D の世界へ！").draw();
+		//マウスの左クリックで描画
+		if (Input::MouseL.pressed)
+		{
+			Line(Mouse::PreviousPos(), Mouse::Pos()).overwrite(image, 30, Palette::White);
 
-		Circle(Mouse::Pos(), 50).draw({ 255, 0, 0, 127 });
+			texture.fill(image.gaussianBlurred(15,15));
+		}
+
+		//NキーでArrayにImageを追加と表示のImageを真っ黒に
+		if (Input::KeyN.pressed)
+		{
+			alhpabet.push_back(image.gaussianBlurred(15, 15));
+			image = Image(Window::Size(), Palette::Black);
+			texture.fill(image);
+		}
+
+		//Cキーで表示をクリアに
+		if (Input::KeyC.pressed)
+		{
+			image = Image(Window::Size(), Palette::Black);
+			texture.fill(image);
+		}
+
+		//SキーでArrayにあるImageを合わせて保存
+		if (Input::KeyS.pressed) 
+		{
+
+			int i = 0, j = 0;
+			for (auto it = alhpabet.begin(); it != alhpabet.end(); it++) 
+			{
+				temp = (*it).scaled(28, 28);
+				dstImage.overwrite(temp, { 28 * i, 28 * j });
+				i++;
+				count++;
+				if (i == 6) {
+					j++;
+					i = 0;
+				}
+
+			}
+			
+
+			dstImage.save(L"./juliamo_alphabet.png");
+		}
+
+		texture.draw();
 	}
 }
